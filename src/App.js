b1162213f56bd326fdf9a6c74ba518d28b5de74d
imkasihf <imkashif@gmail.com>
2020-06-19 17:22:22 +0500

    create basic react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello';
+
+function App(props) {
+  return <div>
+   <p>Hello <strong>{props.myname}</strong>!</p> 
+   <h1>Application Descrpition</h1>
+   <table><tr><td>Application:</td><td>React Native</td></tr>
+   <tr><td>Platform</td><td>Windows</td></tr><br/>
+   <tr><td>Release Dates:</td></tr></table>
+   <ul><li>1.7 2020</li>
+   <li>1.6 2019</li>
+   <li>1.5 2018</li></ul>
+   <h2>Total Number of Releases: {1+2}</h2>
+   <Hello></Hello>
+  </div>
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Hello.css'
+
+function Hello() {
+  return <div className="myname">This is from Hello.js</div>
+}
+
+export default Hello;
